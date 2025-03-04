7bf25594a01e4b7f701deb6c5763c704f85307e5
Adam McKellar <dev@mckellar.eu>
2025-03-04 11:58:25 +0100

    ci: added script to update versions of subdependencies automatically

---
+++ scripts/update-version.bun.ts
@@ -0,0 +1,48 @@
+import { $ } from "bun";
+import { join } from "node:path";
+
+import packageJson from "../package.json";
+
+const sourceFileDir = import.meta.dir;
+const packageRootDir = join(sourceFileDir, "..");
+const packageJsonPath = join(packageRootDir, "package.json");
+
+const currentVersion: string = packageJson.version;
+console.log("Current version: ", currentVersion);
+
+if (Bun.argv.length !== 3) {
+    console.log("USAGE: bun run update-version.bun.ts [patch/minor/major]");
+    process.exit(1);
+}
+if (!["patch", "minor", "major"].includes(Bun.argv[2])) {
+    console.log(
+        `'${Bun.argv[2]}' is not a valid argument. Valid arguments are patch, minor, major`
+    );
+    process.exit(1);
+}
+
+const { stdout } = $`npm --no-git-tag-version version`.cwd(packageRootDir);
+const newVersion = stdout.toString();
+console.log("New version: ", newVersion);
+
+const packageJsonFile = await Bun.file(packageJsonPath);
+const packageJsonFileParsed = await packageJsonFile.json();
+
+packageJsonFileParsed.optionalDependencies[
+    "@nmshd/rs-crypto-node-win32-x64-msvc"
+] = newVersion;
+packageJsonFileParsed.optionalDependencies["@nmshd/rs-crypto-node-darwin-x64"] =
+    newVersion;
+packageJsonFileParsed.optionalDependencies[
+    "@nmshd/rs-crypto-node-darwin-arm64"
+] = newVersion;
+packageJsonFileParsed.optionalDependencies[
+    "@nmshd/rs-crypto-node-linux-x64-gnu"
+] = newVersion;
+
+await Bun.write(
+    packageJsonFile,
+    JSON.stringify(packageJsonFileParsed, null, 2)
+);
+
+console.log("Success!");
